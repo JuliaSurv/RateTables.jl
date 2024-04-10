359d26bf908110e95c1f896ac031685c146414fa
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-04-10 23:29:48 +0200

    Adding a life function, untested

--- src/RateTable.jl
+++ src/RateTable.jl
@@ -114,3 +114,30 @@ The parameters `age` and `date` have to be in days (1 year = $(RT_DAYS_IN_YEAR) 
 @inline daily_hazard(rt::RateTable, a, d; kwargs...) = daily_hazard(getindex(rt; kwargs...), a, d)
 @inline daily_hazard(rt::RateTable, a, d, args...)   = daily_hazard(getindex(rt, args...),   a, d)
 
+
+
+
+
+function get_life(rt,a,d, args...; kwargs...)
+    tbl = getindex(rt, args...;kwargs...)
+    i, j = dty(a, tbl.extrema_age...), dty(d, tbl.extrema_year...)
+
+    rem_a = RT_DAYS_IN_YEAR - rem(a, RT_DAYS_IN_YEAR)
+    rem_d = RT_DAYS_IN_YEAR - rem(d, RT_DAYS_IN_YEAR)
+
+    # Do we go right or below first ? 
+    # happy birthday (below) or happy new year (right) first ?
+    k,l = rem_a < rem_d ? (i+1,j) : (i,j+1)
+
+    # lengths and hazards in the first two cells:  
+    ∂t = [min(rem_a,rem_d), abs(rem_a - rem_d)]
+    λ  = [tbl.values[i,j],  tbl.values[k,l]]
+
+    while (k < size(tbl.values,1)) && (l < size(tbl.values,2))
+        i,j,k,l = i+1, j+1, k+1, l+1
+        push!(∂t, RT_DAYS_IN_YEAR - ∂t[2], ∂t[2])
+        push!(λ, tbl.values[i,j], tbl.values[k,l])
+    end
+    return PiecewiseConstantHazardDistribution(∂t .* RT_YEARS_IN_DAY,λ)
+end
+
