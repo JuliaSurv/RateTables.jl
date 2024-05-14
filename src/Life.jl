3fff09f0842455215b9dee494230734fd08033df
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-05-14 20:55:31 +0200

    up

--- src/Life.jl
+++ src/Life.jl
@@ -1,10 +1,9 @@
 
-struct Life
+struct Life<:ContinuousUnivariateDistribution
     ∂t::Vector{Float64}
     λ::Vector{Float64}
-    function Life(rt,a,d, args...; kwargs...)
-        tbl = getindex(rt, args...;kwargs...)
-        i, j = dty(a, tbl.extrema_age...), dty(d, tbl.extrema_year...)
+    function Life(brt::BasicRateTable,a,d)
+        i, j = dty(a, brt.extrema_age...), dty(d, brt.extrema_year...)
     
         rem_a = RT_DAYS_IN_YEAR - rem(a, RT_DAYS_IN_YEAR)
         rem_d = RT_DAYS_IN_YEAR - rem(d, RT_DAYS_IN_YEAR)
@@ -15,14 +14,14 @@ struct Life
     
         # lengths and hazards in the first two cells:  
         ∂t = [min(rem_a,rem_d), abs(rem_a - rem_d)]
-        λ  = [tbl.values[i,j],  tbl.values[k,l]]
+        λ  = [brt.values[i,j],  brt.values[k,l]]
     
-        while (k < size(tbl.values,1)) && (l < size(tbl.values,2))
+        while (k < size(brt.values,1)) && (l < size(brt.values,2))
             i,j,k,l = i+1, j+1, k+1, l+1
             push!(∂t, RT_DAYS_IN_YEAR - ∂t[2], ∂t[2])
-            push!(λ, tbl.values[i,j], tbl.values[k,l])
+            push!(λ, brt.values[i,j], brt.values[k,l])
         end
-        return new(∂t .* RT_YEARS_IN_DAY,λ)
+        return new(∂t,λ)
     end
 end
 
@@ -30,9 +29,13 @@ function expectation(L::Life)
     S = 1.0
     E = 0.0
     for j in eachindex(L.∂t)
-        S_inc = exp(-L.λ[j]*L.∂t[j])
-        E += S * (1 - S_inc) / L.λ[j]
-        S *= S_inc
+        if L.λ[j] > 0
+            S_inc = exp(-L.λ[j]*L.∂t[j])
+            E += S * (1 - S_inc) / L.λ[j]
+            S *= S_inc
+        else
+            E += S * L.∂t[j]
+        end
     end
     return E
 end
@@ -41,7 +44,7 @@ function cumhazard(L::Life, t)
     u = 0.0
     for j in eachindex(L.∂t)
         u += L.∂t[j]
-        if u > t
+        if t > u 
             Λ += L.λ[j]*L.∂t[j]
         else
             Λ += L.λ[j]*(t-(u-L.∂t[j]))
--- src/RateTables.jl
+++ src/RateTables.jl
@@ -2,6 +2,8 @@ module RateTables
 
 import CSV
 using DataFrames
+using Distributions
+import Distributions: ccdf, expectation
 using Base.Cartesian
 
 const RT_DAYS_IN_YEAR = 365.241
@@ -12,6 +14,19 @@ include("hmd_rates.jl")
 include("r_rates.jl")
 include("Life.jl")
 
-export hmd_rates, hmd_countries, slopop, survexp_us, survexp_usr, survexp_mn, survexp_fr, daily_hazard, availlable_covariates, frpop, Life, expectation, cumhazard, 
+export hmd_rates,
+       hmd_countries,
+       slopop,
+       survexp_us,
+       survexp_usr,
+       survexp_mn,
+       survexp_fr,
+       daily_hazard,
+       availlable_covariates,
+       frpop,
+       Life,
+       expectation,
+       cumhazard,
+       ccdf
 
 end
