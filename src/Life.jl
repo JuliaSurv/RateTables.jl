c041c48478369c02fb491dcaf6d8b971da3fa961
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-05-16 15:16:02 +0200

    docstring correction

--- src/Life.jl
+++ src/Life.jl
@@ -1,7 +1,7 @@
 """
-    Life
+    Life(brt::BasicRateTable,a,d)
 
-This function returns parameters that are used as random variables in survival, expectation, random number generation, etc...
+This function returns a random variable that correspond to an extracted Life from the `BasicRateTable` at age `a` and date `d`. 
 
 This works by checking if the individual is closer to the oldest age than the last year in the ratetable, calculating at each step the time difference and the hazard values. For the younger individuals, we assume they go through the last column at the end no matter what age they are. 
 """
